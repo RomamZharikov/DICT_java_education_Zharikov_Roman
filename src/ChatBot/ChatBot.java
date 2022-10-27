8cabda69352329fff91bddd2aac1ed4581480e3d
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-27 19:50:25 +0300

    Chatbot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -3,7 +3,7 @@ package ChatBot;
 import java.util.Scanner;
 
 public class ChatBot {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello! My name is ZONG.\nI was created in 2022.\nPlease, remind me your name.");
         Scanner name = new Scanner(System.in);
         System.out.printf("What a great name you have, %s!\n", name.nextLine());
@@ -12,7 +12,14 @@ public class ChatBot {
         Scanner fifth = new Scanner(System.in);
         Scanner seven = new Scanner(System.in);
         byte age = (byte) ((three.nextByte() * 70 + fifth.nextByte() * 21 + seven.nextByte() * 15) % 105);
-        System.out.printf("Your age is %d;\nthat's a good time to start programming!", age);
-
+        System.out.printf("Your age is %d;\nthat's a good time to start programming!\n", age);
+        System.out.println("Enter any positive number, I will count from zero to it:");
+        Scanner number = new Scanner(System.in);
+        int number_int = number.nextInt();
+        int i = 0;
+        while (i <= number_int) {
+            System.out.println(i + "!");
+            i++;
+        }
     }
 }
