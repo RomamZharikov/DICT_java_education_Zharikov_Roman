89d72bcb644c40b2fb80c147571ef0ac3dfc67e8
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-27 21:15:52 +0300

    Hangman 2-nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,7 +1,14 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args){
-        System.out.println("HANGMAN\nThe game will be available soon.");
+        System.out.print("HANGMAN\nGuess the word: ");
+        Scanner answer = new Scanner(System.in);
+        if (answer.nextLine().equals("java"))
+            System.out.println("You survived!");
+        else
+            System.out.println("You lost!");
     }
 }
