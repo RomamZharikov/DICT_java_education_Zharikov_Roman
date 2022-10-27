71477eee16d7daa87c0aed88c8fc0274d5115a6d
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-27 21:38:50 +0300

    Hangman 3-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args){
+        String[] list = {
+                "java", "python", "php", "ruby"
+        };
+        String result = list[(int) (Math.random() * list.length)];
         System.out.print("HANGMAN\nGuess the word: ");
         Scanner answer = new Scanner(System.in);
-        if (answer.nextLine().equals("java"))
+        if (answer.nextLine().equals(result))
             System.out.println("You survived!");
         else
             System.out.println("You lost!");
