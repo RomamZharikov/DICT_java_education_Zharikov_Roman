0ccbe46f89942ac7b6d723f892895bffb0d710bc
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-27 18:34:17 +0300

    Chatbot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,7 +1,11 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args){
-        System.out.print("Hello! My name is ZONG.\nI was created in 2022.");
+        System.out.print("Hello! My name is ZONG.\nI was created in 2022.\nPlease, remind me your name.\n");
+        Scanner a = new Scanner(System.in);
+        System.out.print("What a great name you have, " + a.nextLine() + "!");
     }
 }
