00bfd3e4ff5806a4642244f617f6fdad4e58c5fc
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-27 18:56:50 +0300

    Chatbot 3-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args){
-        System.out.print("Hello! My name is ZONG.\nI was created in 2022.\nPlease, remind me your name.\n");
-        Scanner a = new Scanner(System.in);
-        System.out.print("What a great name you have, " + a.nextLine() + "!");
+        System.out.println("Hello! My name is ZONG.\nI was created in 2022.\nPlease, remind me your name.");
+        Scanner name = new Scanner(System.in);
+        System.out.printf("What a great name you have, %s!\n", name.nextLine());
+        System.out.println("Let me guess your age.\nEnter remainders of dividing your age by 3, 5 and 7.");
+        Scanner three = new Scanner(System.in);
+        Scanner fifth = new Scanner(System.in);
+        Scanner seven = new Scanner(System.in);
+        byte age = (byte) ((three.nextByte() * 70 + fifth.nextByte() * 21 + seven.nextByte() * 15) % 105);
+        System.out.printf("Your age is %d;\nthat's a good time to start programming!", age);
+
     }
 }
