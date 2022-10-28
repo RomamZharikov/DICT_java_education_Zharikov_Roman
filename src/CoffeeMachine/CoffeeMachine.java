f3977525fa1f9f4f81f8d4f16cd3870aa7c1055d
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-28 17:55:26 +0300

    CoffeeMachine 3-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -2,9 +2,16 @@ package CoffeeMachine;
 
 public class CoffeeMachine {
     private final int cups_needed;
+    private final int water;
+    private final int milk;
+    private final int coffee_beans;
 
-    public CoffeeMachine(int cups_needed){
+
+    public CoffeeMachine(int cups_needed, int water, int milk, int coffee_beans){
         this.cups_needed = cups_needed;
+        this.milk = milk;
+        this.coffee_beans = coffee_beans;
+        this.water = water;
     }
     public void output_coffee(){
 
@@ -14,5 +21,13 @@ public class CoffeeMachine {
                 %d ml of milk
                 %d g of coffee beans
                 """, cups_needed, cups_needed * 200, cups_needed * 50, cups_needed * 15);
+    }
+    public void can_i_make_coffee(){
+        int made_coffee = (int) Math.floor(Math.min(water/200, Math.min(milk / 50, coffee_beans / 15)));
+        if (made_coffee > cups_needed)
+            System.out.printf("Yes, I can make %d amount coffee (and even %d more than that)\n", cups_needed, made_coffee - cups_needed);
+        else if (made_coffee == cups_needed)
+            System.out.printf("Yes, I can make %d amount of coffee\n", cups_needed);
+        else System.out.printf("No, I can make only %d cups of coffee", made_coffee);
     }
 }
--- src/CoffeeMachine/Main.java
+++ src/CoffeeMachine/Main.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Write how many cups of coffee you will need:");
         Scanner input = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int have_water = input.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int have_milk = input.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int have_coffee_beans = input.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
         int need_coffee = input.nextInt();
-        CoffeeMachine coffee = new CoffeeMachine(need_coffee);
-        coffee.output_coffee();
+        CoffeeMachine coffee = new CoffeeMachine(need_coffee, have_water, have_milk, have_coffee_beans);
+        coffee.can_i_make_coffee();
 
     }
 }
