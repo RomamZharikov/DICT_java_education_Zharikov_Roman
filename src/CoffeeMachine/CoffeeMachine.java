928589c7db362626cd342997a7c9a8a9bb25b862
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-28 21:20:23 +0300

    CoffeeMachine 4-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,33 +1,94 @@
 package CoffeeMachine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
-    private final int cups_needed;
-    private final int water;
-    private final int milk;
-    private final int coffee_beans;
+    private int water;
+    private int milk;
+    private int coffee_beans;
+    private int cups;
+    private int money;
+    private final Scanner input = new Scanner(System.in);
 
 
-    public CoffeeMachine(int cups_needed, int water, int milk, int coffee_beans){
-        this.cups_needed = cups_needed;
-        this.milk = milk;
-        this.coffee_beans = coffee_beans;
-        this.water = water;
+    public CoffeeMachine(){
+        this.milk = 540;
+        this.coffee_beans = 120;
+        this.water = 400;
+        this.cups = 9;
+        this.money = 550;
     }
-    public void output_coffee(){
-
-        System.out.printf("""
-                For %d cups coffee you need:
-                %d ml of water
-                %d ml of milk
-                %d g of coffee beans
-                """, cups_needed, cups_needed * 200, cups_needed * 50, cups_needed * 15);
+    public void buy_menu(){
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino");
+        String choice = input.nextLine();
+            switch (choice) {
+                case "1" -> espresso();
+                case "2" -> latte();
+                case "3" -> cappuccino();
+            }
+        }
+    private void espresso(){
+        System.out.println("\nHow many cups do you want?");
+        int cups_need = input.nextInt();
+        if (cups_need * 250 <= water & cups_need * 16 <= coffee_beans & cups_need <= cups){
+            water -= cups_need * 250;
+            coffee_beans -= cups_need * 16;
+            cups -= cups_need;
+            money += 4;
+            System.out.printf("Take you're %d espresso.", cups_need);
+        }
+        else
+            System.out.println("Please, restock");
     }
-    public void can_i_make_coffee(){
-        int made_coffee = (int) Math.floor(Math.min(water/200, Math.min(milk / 50, coffee_beans / 15)));
-        if (made_coffee > cups_needed)
-            System.out.printf("Yes, I can make %d amount coffee (and even %d more than that)\n", cups_needed, made_coffee - cups_needed);
-        else if (made_coffee == cups_needed)
-            System.out.printf("Yes, I can make %d amount of coffee\n", cups_needed);
-        else System.out.printf("No, I can make only %d cups of coffee", made_coffee);
-    }
+    private void latte(){
+        System.out.println("\nHow many cups do you want?");
+        int cups_need = input.nextInt();
+        if (cups_need * 350 <= water & cups_need * 20 <= coffee_beans & cups_need <= cups & cups_need * 75 <= milk){
+            water -= cups_need * 350;
+            coffee_beans -= cups_need * 20;
+            cups -= cups_need;
+            milk -= 75 * cups_need;
+            money += 7;
+            System.out.printf("Take you're %d latte.", cups_need);
+        }
+        else
+            System.out.println("Please, restock");
+        }
+        private void cappuccino(){
+            System.out.println("\nHow many cups do you want?");
+            int cups_need = input.nextInt();
+            if (cups_need * 200 <= water & cups_need * 12 <= coffee_beans & cups_need <= cups & cups_need * 100 <= milk){
+                water -= cups_need * 200;
+                coffee_beans -= cups_need * 12;
+                cups -= cups_need;
+                milk -= 100 * cups_need;
+                money += 6;
+                System.out.printf("Take you're %d cappuccino.", cups_need);
+            }
+            else
+                System.out.println("Please, restock");
+        }
+        public void fill(){
+            System.out.println("\nWrite how many ml of water you want to add:");
+            water += input.nextInt();
+            System.out.println("Write how many ml of milk you want to add:");
+            milk += input.nextInt();
+            System.out.println("Write how many grams of coffee beans you want to add:");
+            coffee_beans += input.nextInt();
+            System.out.println("Write how many cups you want to add:");
+            cups += input.nextInt();
+        }
+        public void take(){
+            money -= money;
+        }
+        public void out_values(){
+            System.out.printf("""
+                    \nThe coffee machine has:
+                    %dml of water
+                    %dml of milk
+                    %dg of coffee beans
+                    %d of disposable cups
+                    %duah of money
+                    """, water, milk, coffee_beans, cups, money);
+        }
 }
--- src/CoffeeMachine/Main.java
+++ src/CoffeeMachine/Main.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int have_water = input.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int have_milk = input.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int have_coffee_beans = input.nextInt();
-        System.out.println("Write how many cups of coffee you will need:");
-        int need_coffee = input.nextInt();
-        CoffeeMachine coffee = new CoffeeMachine(need_coffee, have_water, have_milk, have_coffee_beans);
-        coffee.can_i_make_coffee();
+        CoffeeMachine coffee = new CoffeeMachine();
+        coffee.out_values();
+        System.out.println("\nWrite action (buy, fill, take):");
+        String choice = input.nextLine();
+        switch (choice){
+            case "buy" -> coffee.buy_menu();
+            case "fill" -> coffee.fill();
+            case  "take" -> coffee.take();
+        }
+        coffee.out_values();
 
     }
 }
