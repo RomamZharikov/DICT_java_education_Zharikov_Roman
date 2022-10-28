38a3a93e8d720eb02521be818b2764bb151e0960
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-28 09:48:30 +0300

    Hangman 5-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,21 +1,49 @@
 package Hangman;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        byte life = 8;
         String[] list = {
                 "java", "python", "kotlin", "javascript"
         };
         String result = list[(int) (Math.random() * list.length)];
-        char[] result_array = result.toCharArray();
-        for (int i = 2; i < result_array.length; i++)
-            result_array[i] = '-';
-        System.out.printf("HANGMAN\nGuess the word %s: ", String.valueOf(result_array));
-        Scanner answer = new Scanner(System.in);
-        if (answer.nextLine().equals(result))
-            System.out.println("You survived!");
+        char[] result_array =  result.toCharArray();
+        char[] result_char = new char[result_array.length];
+        for (int i = 0; i < result_array.length; i++)
+            result_char[i] = '-';
+        Scanner input = new Scanner(System.in);
+        while (life > 0) {
+            System.out.printf("You life: %d\nGuess the word %s: ", life, String.valueOf(result_char));
+            char char_answer = input.nextLine().toCharArray()[0];
+            char[] new_result_char = checkout(result_array, result_char, char_answer);
+            if (new_result_char == null)
+                life--;
+            else if (Arrays.equals(new_result_char, result_array)) {
+                System.out.println("You win!!!");
+                break;
+            }
+            else
+                result_char = new_result_char;
+        }
+        if (life <= 0)
+            System.out.println("You lose!!!");
+
+    }
+    public static char[] checkout(char[] result, char[] result_char, char answer){
+        byte changed = 0;
+        for (int i = 0; i < result_char.length; i++) {
+            if (answer == result[i]) {
+                result_char[i] = answer;
+                changed++;
+            }
+        }
+        if (changed != 0)
+            return result_char;
         else
-            System.out.println("You lost!");
+            return null;
     }
 }
