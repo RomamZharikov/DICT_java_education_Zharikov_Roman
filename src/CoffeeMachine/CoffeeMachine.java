f5ccafe50460257c5814038c4a2bd299674e0c24
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-29 13:12:31 +0300

    CoffeeMachine 5-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -19,64 +19,40 @@ public class CoffeeMachine {
         this.money = 550;
     }
     public void buy_menu(){
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino");
-        String choice = input.nextLine();
+        byte try_choice = 0;
+        while (try_choice == 0) {
+            System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to exit in main menu");
+            String choice = input.nextLine();
             switch (choice) {
-                case "1" -> espresso();
-                case "2" -> latte();
-                case "3" -> cappuccino();
+                case "1" -> {espresso(); try_choice += 1;}
+                case "2" -> {latte(); try_choice += 1;}
+                case "3" -> {cappuccino(); try_choice += 1;}
+                case "back" -> try_choice += 1;
+                default -> System.out.println("Unknown variable: " + choice + "\nTry again!");
             }
         }
+    }
     private void espresso(){
         System.out.println("\nHow many cups do you want?");
-        int cups_need = input.nextInt();
-        if (cups_need * 250 <= water & cups_need * 16 <= coffee_beans & cups_need <= cups){
-            water -= cups_need * 250;
-            coffee_beans -= cups_need * 16;
-            cups -= cups_need;
-            money += 4;
-            System.out.printf("Take you're %d espresso.", cups_need);
-        }
-        else
-            System.out.println("Please, restock");
+            coffee_preparation(Integer.parseInt(input.nextLine()), 250, 0, 16, 4, "espresso");
     }
     private void latte(){
         System.out.println("\nHow many cups do you want?");
-        int cups_need = input.nextInt();
-        if (cups_need * 350 <= water & cups_need * 20 <= coffee_beans & cups_need <= cups & cups_need * 75 <= milk){
-            water -= cups_need * 350;
-            coffee_beans -= cups_need * 20;
-            cups -= cups_need;
-            milk -= 75 * cups_need;
-            money += 7;
-            System.out.printf("Take you're %d latte.", cups_need);
-        }
-        else
-            System.out.println("Please, restock");
-        }
+        coffee_preparation(Integer.parseInt(input.nextLine()), 350, 75, 20, 7, "latte");
+    }
         private void cappuccino(){
             System.out.println("\nHow many cups do you want?");
-            int cups_need = input.nextInt();
-            if (cups_need * 200 <= water & cups_need * 12 <= coffee_beans & cups_need <= cups & cups_need * 100 <= milk){
-                water -= cups_need * 200;
-                coffee_beans -= cups_need * 12;
-                cups -= cups_need;
-                milk -= 100 * cups_need;
-                money += 6;
-                System.out.printf("Take you're %d cappuccino.", cups_need);
-            }
-            else
-                System.out.println("Please, restock");
+            coffee_preparation(Integer.parseInt(input.nextLine()), 200, 100, 12, 6, "cappuccino");
         }
         public void fill(){
             System.out.println("\nWrite how many ml of water you want to add:");
-            water += input.nextInt();
+            water += Integer.parseInt(input.nextLine());
             System.out.println("Write how many ml of milk you want to add:");
-            milk += input.nextInt();
+            milk += Integer.parseInt(input.nextLine());
             System.out.println("Write how many grams of coffee beans you want to add:");
-            coffee_beans += input.nextInt();
+            coffee_beans += Integer.parseInt(input.nextLine());
             System.out.println("Write how many cups you want to add:");
-            cups += input.nextInt();
+            cups += Integer.parseInt(input.nextLine());
         }
         public void take(){
             money -= money;
@@ -88,7 +64,27 @@ public class CoffeeMachine {
                     %dml of milk
                     %dg of coffee beans
                     %d of disposable cups
-                    %duah of money
+                    %d uah of money
                     """, water, milk, coffee_beans, cups, money);
+        }
+        private void coffee_preparation(int cups_needed, int water_need, int milk_need, int coffee_beans_need, int money_take, String name){
+            if (cups_needed * water_need <= water){
+                if (cups_needed * coffee_beans_need <= coffee_beans){
+                    if (cups_needed <= cups){
+                        if (cups_needed * milk_need <= milk){
+                            water -= cups_needed * water_need;
+                            coffee_beans -= cups_needed * coffee_beans_need;
+                            cups -= cups_needed;
+                            milk -= milk_need * cups_needed;
+                            money += money_take;
+                            System.out.printf("Take you're %d %s.", cups_needed, name);
+                        } else
+                            System.out.println("Please, restock milk");
+                    } else
+                        System.out.println("Please, restock cups");
+                } else
+                    System.out.println("Please, restock coffee beans");
+            } else
+                System.out.println("Please, restock water");
         }
 }
--- src/CoffeeMachine/Main.java
+++ src/CoffeeMachine/Main.java
@@ -6,15 +6,18 @@ public class Main {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         CoffeeMachine coffee = new CoffeeMachine();
-        coffee.out_values();
-        System.out.println("\nWrite action (buy, fill, take):");
-        String choice = input.nextLine();
-        switch (choice){
-            case "buy" -> coffee.buy_menu();
-            case "fill" -> coffee.fill();
-            case  "take" -> coffee.take();
+        byte exit = 0;
+        while (exit == 0) {
+            System.out.println("\nWrite action (buy, fill, take, remaining or exit):");
+            String choice = input.nextLine();
+            switch (choice) {
+                case "buy" -> coffee.buy_menu();
+                case "fill" -> coffee.fill();
+                case "take" -> coffee.take();
+                case "remaining" -> coffee.out_values();
+                case "exit" -> exit = 1;
+                default -> System.out.println("Unknown variable: " + choice);
+            }
         }
-        coffee.out_values();
-
     }
 }
