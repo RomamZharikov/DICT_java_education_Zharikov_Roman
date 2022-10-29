c5e0a8bcc0f4896627ac7d7ea1658db4900cf1f2
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-10-29 16:28:01 +0300

    ChatBot 6-th stage (correcting mistakes)

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -3,25 +3,29 @@ package ChatBot;
 import java.util.Scanner;
 
 public class ChatBot {
-    public static void main(String[] args) {
-        System.out.println("\nHello! My name is ZONG.\nI was created in 2022.\nPlease, remind me your name.");
-        Scanner name = new Scanner(System.in);
-        System.out.printf("What a great name you have, %s!\n", name.nextLine());
+    private static final Scanner input = new Scanner(System.in);
+    private final String name;
+    public ChatBot(String name){
+        this.name = name;
+    }
+    public void chat_bot_main(){
+        System.out.printf("\nHello! My name is %s.\nI was created in 2022.\nPlease, remind me your name.\n", name);
+        System.out.printf("What a great name you have, %s!\n", input.nextLine());
         System.out.println("\nLet me guess your age.\nEnter remainders of dividing your age by 3, 5 and 7.");
-        Scanner three = new Scanner(System.in);
-        Scanner fifth = new Scanner(System.in);
-        Scanner seven = new Scanner(System.in);
-        byte age = (byte) ((three.nextByte() * 70 + fifth.nextByte() * 21 + seven.nextByte() * 15) % 105);
-        System.out.printf("Your age is %d;\nthat's a good time to start programming!\n", age);
+        System.out.printf("Your age is %d;\nthat's a good time to start programming!\n", guess_age(input.nextByte(), input.nextByte(), input.nextByte()));
         System.out.println("\nEnter any positive number, I will count from zero to it:");
-        Scanner number = new Scanner(System.in);
-        int number_int = number.nextInt();
-        int i = 0;
-        while (i <= number_int) {
-            System.out.println(i + "!");
-            i++;
-        }
+        count_to_zero(input.nextInt());
         System.out.println("\nLet`s have a quiz.");
+        guess();
+    }
+    private byte guess_age(byte three, byte fifth, byte six){
+        return (byte) ((three * 70 + fifth * 21 + six * 15) % 105);
+    }
+    private void count_to_zero(int number_int){
+        for (int i = 0; i <= number_int; i++)
+            System.out.println(i + "!");
+    }
+    private void guess(){
         while (true){
             System.out.println("""
                     What language am I(ChatBot) created?
@@ -39,3 +43,4 @@ public class ChatBot {
         }
     }
 }
+
